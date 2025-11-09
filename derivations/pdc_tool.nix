be39652b291be99c087cdd0931d7c104ca39eb17
Ruby Iris Juric <ruby@srxl.me>
2025-11-09 14:10:28 +0800

    pdc_tool: 0.3.1 -> 0.3.4

--- derivations/pdc_tool.nix
+++ derivations/pdc_tool.nix
@@ -5,20 +5,20 @@
 }:
 
 let
-  version = "0.3.1";
+  version = "0.3.4";
 
   binaryArchive = {
     x86_64-linux = fetchzip {
       url = "https://github.com/HBehrens/pdc_tool/releases/download/v${version}/pdc_tool_linux_x86_64-unknown-linux-musl.zip";
-      hash = "sha256-J9P+EJKSw6oqptEPQN/xWEXvFd6seX3UQKLMw9Eac4U=";
+      hash = "sha256-AChRPjcj2WdHMjYzBQwZ8Fe8LuryavVHqS7SvqNwtDg=";
     };
     x86_64-darwin = fetchzip {
       url = "https://github.com/HBehrens/pdc_tool/releases/download/v${version}/pdc_tool_macos_x86_64-apple-darwin.zip";
-      hash = "sha256-E4PvbYVa8ntcR9aQjtD2whNgYowr1NWOQJu88AiGeyk=";
+      hash = "sha256-lcYa9RoQpsXXwr9S3yvOe/AyCIgOHXdHpf80LRA7vPo=";
     };
     aarch64-darwin = fetchzip {
       url = "https://github.com/HBehrens/pdc_tool/releases/download/v${version}/pdc_tool_macos_aarch64-apple-darwin.zip";
-      hash = "sha256-2DEZ9MJgfWCtWQLik1HqrdklDRh+YerUgjYnwGSNq68=";
+      hash = "sha256-4dHR4VP9twR0PyXMAICOOU8Lcu6Q/y4dWQIXe54YcYk=";
     };
   };
 in
