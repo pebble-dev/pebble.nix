e6086f2790f17968dcd5b89935100b20d3313a5c
Ruby Iris Juric <ruby@foxgirl.engineering>
2026-08-26 21:44:31 +1000

    pebble-toolchain-bin: 4.9.169 -> 4.33.1

--- derivations/pebble-toolchain-bin.nix
+++ derivations/pebble-toolchain-bin.nix
@@ -7,23 +7,23 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "pebble-toolchain-bin";
-  version = "4.9.169";
+  version = "4.33.1";
 
   src =
     ({
       x86_64-linux = fetchzip {
         url = "https://sdk.repebble.com/releases/${finalAttrs.version}/toolchain-linux-x86_64.tar.gz";
-        hash = "sha256-tz1DvYe9I262BfZsZNauII893yQf5QHlvQ3TpWG17vQ=";
+        hash = "sha256-lJgmri289HETYfJkwywCdJ1V/yD8WSItLq7Apvc0j1k=";
         stripRoot = false;
       };
       x86_64-darwin = fetchzip {
         url = "https://sdk.repebble.com/releases/${finalAttrs.version}/toolchain-mac-x86_64.tar.gz";
-        hash = "sha256-1672hlLCs2cLaZPnTt0J92MgmcIA1OQMvTeVKWTuTUs=";
+        hash = "sha256-Vjw++ijNQTNcHd4UYgi8iudn9qG6CG3qNP+uM8xIXZM=";
         stripRoot = false;
       };
       aarch64-darwin = fetchzip {
         url = "https://sdk.repebble.com/releases/${finalAttrs.version}/toolchain-mac-arm64.tar.gz";
-        hash = "sha256-Qb+yqbXbWUOi188eh3jZ84rhqzY9MO/B2FawTQKAYJ0=";
+        hash = "sha256-4KcQTJr9nrm97ZEgwbu5rxenWeizNMnI7C4tFbiQpaM=";
         stripRoot = false;
       };
     }).${stdenv.hostPlatform.system};
